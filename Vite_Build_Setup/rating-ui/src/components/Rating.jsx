56be29cc3b133d0511104257aabc52e7c6f46728
zieDoo <samuel.bednarikall@gmail.com>
2026-09-07 20:45:43 +0200

    React Fundamentals - Inline Styling

--- Vite_Build_Setup/rating-ui/src/components/Rating.jsx
+++ Vite_Build_Setup/rating-ui/src/components/Rating.jsx
@@ -1,5 +1,28 @@
+// There is many different ways to style your components in React.
+
+// 1) Inline styling
+
+// using 'style' tag. We have to use double curly braces {{}}. Because we are putting there a javascript not a CSS. Outter braces saying it will be JS/dynamic content. Inner braces saying it will be object.Here we using cammelCase 'textAlign' syntax because it is JS object. Usual CSS syntax is with dash: 'text-align'
+
 const Rating = () => {
-  return <div>Rate Your Experience</div>;
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        fontFamily: "Arial",
+        padding: "20px",
+      }}
+    >
+      <h2>Rate Your Experience</h2>
+    </div>
+  );
 };
 
 export default Rating;
+
+// 1) Inline styling
+// 2) Style Object
+// 3) Plain CSS
+// 4) CSS modules
+// 5) CSS framework Tailwind
+// 6) Style Components
