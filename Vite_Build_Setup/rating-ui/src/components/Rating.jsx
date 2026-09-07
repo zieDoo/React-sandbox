7592454d92d1d645ad28634f040b46c7d3e5ba5a
zieDoo <samuel.bednarikall@gmail.com>
2026-09-07 20:59:26 +0200

    React Fundamentals - Styling Object

--- Vite_Build_Setup/rating-ui/src/components/Rating.jsx
+++ Vite_Build_Setup/rating-ui/src/components/Rating.jsx
@@ -4,21 +4,10 @@
 
 const Rating = () => {
   return (
-    <div style={styles.container}>
-      <h2 style={styles.heading}>Rate Your Experience</h2>
+    <div>
+      <h2>Rate Your Experience</h2>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    textAlign: "center",
-    fontFamily: "Arial",
-    padding: "20px",
-  },
-  heading: {
-    color: "red",
-  },
 };
 
 export default Rating;
