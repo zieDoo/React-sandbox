f6bf27e8f2fb7a7031115cee991f1c64e316a4b4
zieDoo <samuel.bednarikall@gmail.com>
2026-09-08 11:50:41 +0200

    React Fundamentals - Lists + keys

--- Vite_Build_Setup/rating-ui/src/components/Rating.jsx
+++ Vite_Build_Setup/rating-ui/src/components/Rating.jsx
@@ -1,23 +1,35 @@
-// There is many different ways to style your components in React.
-
-// 3) Plain CSS - usual way
-
 const Rating = () => {
+  // For dynamic array
+  // we dont need a value so using a '_' placeholder.
+  // i - is for the index
+  // returning i + 1, so it just increments.
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
+      <div className="stars">
+        {stars.map(
+          (
+            star, // use stars instead of hardcoded array with numbers.
+          ) => (
+            // {[1, 2, 3, 4, 5].map((star) => (
+            //   <span>{star}</span> // we wan to render something base on each of these items/stars. star is 1,2,...
+            // But we can use UNICODE characters: '\u2605' = star
+
+            // !!! we have to add a key to span !!!
+            <span key={star} className="star">
+              {"\u2605"}
+            </span> // we also add className to our span
+          ),
+        )}
+      </div>
     </div>
   );
 };
 
 export default Rating;
 
-// 1) Inline styling
+// We can create a list using with map method. List in react is simply a group of items. Usually you gona render some kind of JSX output per item.
 
-// using 'style' tag. We have to use double curly braces {{}}. Because we are putting there a javascript not a CSS. Outter braces saying it will be JS/dynamic content. Inner braces saying it will be object.Here we using cammelCase 'textAlign' syntax because it is JS object. Usual CSS syntax is with dash: 'text-align'
-
-// 2) Style Object
-// 3) Plain CSS
-// 4) CSS modules
-// 5) CSS framework Tailwind
-// 6) Style Components
+// If we creating a list in React, we need to add a key to element we rendering. otherwise we get a warning: 'Each child in a list should have a unique "key" prop.'
