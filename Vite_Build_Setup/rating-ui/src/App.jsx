1132a04a9f93e51e7eb30ad4b9cea1441d504092
zieDoo <samuel.bednarikall@gmail.com>
2026-09-11 10:15:25 +0200

    React Fundamentals - More on useState Hook - Pass function to Setter

--- Vite_Build_Setup/rating-ui/src/App.jsx
+++ Vite_Build_Setup/rating-ui/src/App.jsx
@@ -5,11 +5,22 @@ const Counter = () => {
   //   const [count, setCount] = useState(0);    // Our default value can be also a function. We can do something in our initial state. But we need to return our initial state value (0 in our case). And whatever we return from this, is gona be a value that is saved as default for this piece of state.
   const [count, setCount] = useState(() => {
     console.log("Init count state...");
-    return 9;
+    return 0;
   });
+
+  // In setter (setCount), we can also pass a function
+
+  const increment = () => {
+    // setCount(count + 1); // adding +1 when we click the button
+    // Issue is, if we using a previous state (count), like in our case.
+    // We shouldnt do it this way. We should pass a function
+    setCount(count + 1);
+  };
+
   return (
     <>
       <h2>Counter: {count}</h2>
+      <button onClick={increment}>Increment</button>
     </>
   );
 };
