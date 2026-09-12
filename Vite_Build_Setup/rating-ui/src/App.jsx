62f581998b5b2e5a9939157aec5bb4544dd3f7f8
zieDoo <samuel.bednarikall@gmail.com>
2026-09-12 19:13:08 +0200

    React Fundamentals - Adding another default values to Rating component

--- Vite_Build_Setup/rating-ui/src/App.jsx
+++ Vite_Build_Setup/rating-ui/src/App.jsx
@@ -4,7 +4,17 @@ const App = () => {
   return (
     <div>
       {/* If we pass props as heading here - we will overide default value in our component  */}
-      <Rating heading="How do you feel about React?" />
+      <Rating
+        heading="How do you feel about React?"
+        color="blue"
+        feedbackMessages={[
+          "Hate it",
+          "Dislike it",
+          "Meh",
+          "Like it",
+          "Love it",
+        ]}
+      />
     </div>
   );
 };
--- Vite_Build_Setup/rating-ui/src/components/Rating.jsx
+++ Vite_Build_Setup/rating-ui/src/components/Rating.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 
 // We can also have a default value, if nothing is passed in props in main App.
-const Rating = ({ heading = "Rate your Experience" }) => {
+// We added props 'color' with default value gold. Also default value for feedbackmessages.
+const Rating = ({
+  heading = "Rate your Experience",
+  color = "gold",
+  feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"],
+}) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
-
-  const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
 
   return (
     <div className="rating-container">
@@ -21,7 +24,17 @@ const Rating = ({ heading = "Rate your Experience" }) => {
             onMouseEnter={() => setHover(star)}
             onMouseLeave={() => setHover(0)} // back to original state
             key={star}
-            className={`star ${star <= (hover || rating) ? "active" : ""}`}
+            // Now the gold color is comming from the class of 'active'.
+            // But we want to use color passed in as props (red). So instead of using dynamic class we just use inline styling so we can use variable.
+
+            // className={`star ${star <= (hover || rating) ? "active" : ""}`}
+            className="star" // we want to keep a star class
+            style={{
+              // and we use inline styling
+              // if star is less then and equal to hover or rating, use red color from props (red)
+              // else grey color '#ccc'
+              color: star <= (hover || rating) ? color : "#ccc",
+            }}
           >
             {"\u2605"}
           </span>
