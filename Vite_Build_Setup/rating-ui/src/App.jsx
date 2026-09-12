30497a5c83aba2fd81c031b09c6499618359df82
zieDoo <samuel.bednarikall@gmail.com>
2026-09-12 15:55:17 +0200

    React Fundamentals - Basic using of Props

--- Vite_Build_Setup/rating-ui/src/App.jsx
+++ Vite_Build_Setup/rating-ui/src/App.jsx
@@ -1,9 +1,14 @@
 import Rating from "./components/Rating";
 
+// Props - is way to pass data from a parent component to a child component.
+// They are similar to attributes in HTML.
+
 const App = () => {
   return (
     <div>
-      <Rating />
+      {/* In our main App component we can put our prop called with whatever name.
+  We decided to pass props called 'heading' with string. */}
+      <Rating heading="How do you feel about React?" />
     </div>
   );
 };
--- Vite_Build_Setup/rating-ui/src/components/Rating.jsx
+++ Vite_Build_Setup/rating-ui/src/components/Rating.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
 
-const Rating = () => {
+// To use a prop
+// Here we taking our props as an argument
+const Rating = (props) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
-  // to show messages
-  // what to put and what dont put to the state.
-  // these messages are not gona change. So we dont put them in state.
-  // state is for data that is to be watched and will update the UI if it is changed.
   const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
 
   return (
     <div className="rating-container">
-      <h2>Rate Your Experience</h2>
+      {/* Here we can use our props argument to have dynamic behavior*/}
+      <h2>{props.heading}</h2>
       <div className="stars">
         {stars.map((star) => (
           <span
@@ -22,16 +21,13 @@ const Rating = () => {
             onMouseEnter={() => setHover(star)}
             onMouseLeave={() => setHover(0)} // back to original state
             key={star}
-            // we want to do a conditional styling here, so choosed star get an active css class to have golden rating. class star should be there by default. Active class is dynamic, it depends on the state. So we make className dynamic.
             className={`star ${star <= (hover || rating) ? "active" : ""}`}
           >
             {"\u2605"}
           </span>
         ))}
       </div>
-      {/* here we output our rating messages */}
-      {/* if rating is 0 we dont want to show any message.
-      Also we are using short circuit rendering here, if what's in the left is true, then lets show what's the right. So class feedback (we have in CSS. and show the feedbackMessages array with index where whatever rating is minus 1. Minus one because index is 0 based ). Stars starting with 1,2,3.... But messages are going from 0,1,2... */}
+
       {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
     </div>
   );
